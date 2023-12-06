df967b725b24c8f9f2abb0be4e0da1987af11e5d
Foysal Ahmed <mfoysal314@gmail.com>
2023-12-07 00:58:13 +0600

    user create serializer added and customer djoser serializer in settings

--- test_project/settings.py
+++ test_project/settings.py
@@ -153,4 +153,11 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
+
 AUTH_USER_MODEL = 'core.User'
+
+DJOSER = {
+    'SERIALIZERS':{
+        'user_create' : 'core.serializers.UserCreateSerializer'
+    }
+}
