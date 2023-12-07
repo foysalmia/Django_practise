ef45114de6eefe206d906716b234daa3c9b50631
Foysal Ahmed <mfoysal314@gmail.com>
2023-12-08 01:15:46 +0600

    creating order serializer is done

--- store/serializers.py
+++ store/serializers.py
@@ -107,3 +107,13 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Order
         fields = ['id','customer','payment_status','items']
+
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def save(self, *args, **kwargs):
+       print(self.validated_data['cart_id'])
+       print(self.context['user_id'])
+       (customer,created) = Customer.objects.get_or_create(user_id = self.context['user_id'])
+       Order.objects.create(customer= customer)
+       
