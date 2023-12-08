44c2d9d20b8c611d55087f92a00fed37226afe99
Foysal Ahmed <mfoysal314@gmail.com>
2023-12-09 02:53:22 +0600

    signal added and getorcreated method removed

--- store/serializers.py
+++ store/serializers.py
@@ -127,7 +127,7 @@ class CreateOrderSerializer(serializers.Serializer):
     def save(self, *args, **kwargs):
        with transaction.atomic():
             cart_id = self.validated_data['cart_id']
-            (customer,created) = Customer.objects.get_or_create(user_id = self.context['user_id'])
+            customer = Customer.objects.get(user_id = self.context['user_id'])
             order = Order.objects.create(customer= customer)
             cart_items = CartIteam.objects.select_related('product').filter(cart_id = cart_id)
             order_item = [
